95d47eff0c5b38435bf37d432e8b806744a511f5
Walluttt <tudwallcreze@gmail.com>
2025-11-12 22:36:34 +0100

    final corr

--- verifier_invariant.py
+++ verifier_invariant.py
@@ -1,32 +1,32 @@
 from collections import deque
-from typing import Set, Dict, List, Tuple, Callable
-from TransitionSystem import State, TransitionSystem, SatisfactionFunction 
-from properties import check_satisfaction 
-
+from typing import Set, List, Tuple
+from TransitionSystem import State, TransitionSystem, SatisfactionFunction
+from properties import check_satisfaction
 
 def invariant_checker(ts: TransitionSystem, phi_func: SatisfactionFunction) -> Tuple[bool, List[State]]:
     R: Set[State] = set()
     U: deque[State] = deque()
-    violating: State | None = None
+    b = True  # invariant global
 
+    def visiter(s: State) -> None:
+        nonlocal b
+        U.append(s)
+        R.add(s)
+        while U and b:
+            s_prime = U[-1]
+            post = ts.get_post(s_prime) - R
+            if not post:
+                s = U.pop()
+                b = b and check_satisfaction(s_prime, ts, phi_func)
+            else:
+                s_double_prime = post.pop()
+                U.append(s_double_prime)
+                R.add(s_double_prime)
+        if(not b):
+            print("l'état de violation est: ", s)
     for s0 in ts.I - R:
-        U.append(s0)
-        R.add(s0)
-
-        while U and violating is None:
-            current = U[-1]
-            news = ts.get_post(current) - R
-
-            if not news:                       # tous les successeurs vus
-                U.pop()
-                if not check_satisfaction(current, ts, phi_func):
-                    violating = current
-            else:                              # explorer un nouvel état
-                nxt = news.pop()
-                U.append(nxt)
-                R.add(nxt)
-
-        if violating is not None:
-            return False, list(U) + [violating]
+        visiter(s0)
+        if not b:
+            return False, list(U)
 
     return True, []
